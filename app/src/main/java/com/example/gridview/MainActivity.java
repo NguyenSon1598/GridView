5cbd117512a39ee73571ec79abd9bf9a55c1c942
Nguyenson1598 <nhoxblood@gmail.com>
2019-09-27 10:35:46 -0700

    Initial commit

---
+++ app/src/main/java/com/example/gridview/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.gridview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+
+public class MainActivity extends AppCompatActivity {
+    String arr[]={"Ipad","Iphone","New Ipad","SamSung","Nokia","Sony Ericson","LG","Q-Mobile","HTC","HK Phone,","Blackerry",
+            "G Phone","FPT-phone"};
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView selection = (TextView) findViewById(R.id.Selection);
+            final GridView gv =(GridView)findViewById(R.id.gridView1);
+            ArrayAdapter<String> da = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arr);
+        gv.setAdapter(da);
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                selection.setText(arr[i]);
+            }
+        });
+    }
+}
